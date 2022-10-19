8c100a5d42eb08c58cd76b2d97046c1f9427c465
Nguyen Thanh Hai <haint@devcamp.edu.vn>
2022-10-19 14:14:08 +0700

    fix scroll top btn

--- src/components/GoToTop/GoToTop.js
+++ src/components/GoToTop/GoToTop.js
@@ -1,20 +1,38 @@
 import PropTypes from 'prop-types'
 import classNames from "classnames/bind"
-import styles from './GoToTop.module.scss'
 import { RiArrowUpLine } from 'react-icons/ri'
+import { useEffect, useState } from 'react'
+
+import styles from './GoToTop.module.scss'
 
 const cx = classNames.bind(styles)
 
 function GoToTop({ left }) {
+    const [show, setShow] = useState(false)
+
+    useEffect(() => {
+        const showGoToTopBtn = () => {
+            const scrollY =  window.scrollY
+            scrollY > 100 ? setShow(true) : setShow(false)
+        }
+
+        window.addEventListener('scroll', showGoToTopBtn)
+
+        return () => {
+            window.removeEventListener('scroll', showGoToTopBtn)
+        }
+    }, [])
 
     const goToTop = () => {
         document.documentElement.scrollTop = 0
     }
 
     return (
-        <div className={cx('wrapper', {left})} onClick={() => goToTop()}>
-            <RiArrowUpLine />
-        </div>
+        <>
+            {show && <div className={cx('wrapper', {left})} onClick={() => goToTop()}>
+                <RiArrowUpLine />
+            </div>}
+        </>
     );
 }
 
